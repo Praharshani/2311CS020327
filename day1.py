655b89d8ddc2e519195a5cbd4cad703c30ded04b
Praharshani <kodumurupraharshani@gmail.com>
2025-01-07 15:45:13 +0000

    any name about project

---
+++ day1.py
@@ -0,0 +1,17 @@
+integer_var = 10          
+float_var = 3.14           
+string_var = "Hello, World!" 
+boolean_var = True         
+
+
+print("Integer Variable: ", integer_var)
+print("Type of integer_var: ", type(integer_var))
+
+print("\nFloat Variable: ", float_var)
+print("Type of float_var: ", type(float_var))
+
+print("\nString Variable: ", string_var)
+print("Type of string_var: ", type(string_var))
+
+print("\nBoolean Variable: ", boolean_var)
+print("Type of boolean_var: ", type(boolean_var))
