e648fb5fe6e0b6e52be161443eb994b9a3305616
Praharshani <kodumurupraharshani@gmail.com>
2025-01-07 15:54:05 +0000

    any name about project

---
+++ day2.py
@@ -0,0 +1,18 @@
+my_list = [10, 20, 30, 40, 50]
+my_tuple = (100, 200, 300, 400, 500)
+my_dict = {"apple": 1, "banana": 2, "cherry": 3, "date": 4, "elderberry": 5}
+list_element_1 = my_list[2]
+list_element_2 = my_list[-1]
+tuple_element_1 = my_tuple[0]
+tuple_element_2 = my_tuple[3]  
+dict_element_1 = my_dict["banana"]  
+dict_element_2 = my_dict["elderberry"] 
+print("List:", my_list)
+print("Element at index 2 in List:", list_element_1)
+print("Element at the last index in List:", list_element_2)
+print("\nTuple:", my_tuple)
+print("Element at index 0 in Tuple:", tuple_element_1)
+print("Element at index 3 in Tuple:", tuple_element_2)
+print("\nDictionary:", my_dict)
+print("Value for 'banana' in Dictionary:", dict_element_1)
+print("Value for 'elderberry' in Dictionary:", dict_element_2)
